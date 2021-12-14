f80fb10b560d04f44b8831021c29254597d3f839
Twiz0x <twizox@gmail.com>
2021-12-14 21:06:29 +0100

    gamestate

--- src/main/java/net/hydramc/domination/Domination.java
+++ src/main/java/net/hydramc/domination/Domination.java
@@ -1,7 +1,21 @@
 package net.hydramc.domination;
 
+import net.hydramc.domination.handlers.GameState;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Domination extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        Bukkit.getLogger().info("[Domination] On");
+        GameState.setState(GameState.WAITTING);
+    }
+
+    @Override
+    public void onDisable() {
+        GameState.setState(GameState.OFF);
+        Bukkit.getLogger().info("[Domination] Off");
+    }
 
 }
