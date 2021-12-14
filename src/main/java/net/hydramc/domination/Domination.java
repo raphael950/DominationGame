aa9156064704aec1a70e8f1f587edfab425485cc
MrCubee <contact@mrcubee.fr>
2021-12-14 21:09:29 +0100

    Replace Bukkit's logger with Plugin's logger.

--- src/main/java/net/hydramc/domination/Domination.java
+++ src/main/java/net/hydramc/domination/Domination.java
@@ -1,21 +1,20 @@
 package net.hydramc.domination;
 
 import net.hydramc.domination.handlers.GameState;
-import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Domination extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        Bukkit.getLogger().info("[Domination] On");
+        getLogger().info("[Domination] On");
         GameState.setState(GameState.WAITTING);
     }
 
     @Override
     public void onDisable() {
         GameState.setState(GameState.OFF);
-        Bukkit.getLogger().info("[Domination] Off");
+        getLogger().info("[Domination] Off");
     }
 
 }
