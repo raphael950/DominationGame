8edda61428e06572377d125ab79a181022267dd1
MrCubee <contact@mrcubee.fr>
2021-12-26 20:00:57 +0100

    Complete EscortedEntity's spawnEntity method.

--- src/main/java/net/hydramc/domination/entity/EscortedEntity.java
+++ src/main/java/net/hydramc/domination/entity/EscortedEntity.java
@@ -1,17 +1,30 @@
 package net.hydramc.domination.entity;
 
 import net.hydramc.domination.team.Team;
+import net.hydramc.util.Version;
 import org.bukkit.Location;
 import org.bukkit.entity.IronGolem;
 import org.bukkit.event.entity.CreatureSpawnEvent;
+
+import java.lang.reflect.Method;
 
 public interface EscortedEntity extends IronGolem {
 
     public Team getTeamTarget();
     public void setTeamTarget(Team team);
-    
+
     public static EscortedEntity spawnEntity(Location location, CreatureSpawnEvent.SpawnReason spawnReason) {
-        //TODO: Call CraftEscortedEntity static NMS method.
+        final Class<?> craftEscortedEntityClass;
+        final Method method;
+
+        craftEscortedEntityClass = Version.getNMSClass("net.hydramc.domination.entity.%s.CraftEscortedEntity");
+        if (craftEscortedEntityClass == null)
+            return null;
+        try {
+            method = craftEscortedEntityClass.getDeclaredMethod("spawnEntity", Location.class, CreatureSpawnEvent.SpawnReason.class);
+            method.setAccessible(true);
+            return (EscortedEntity) method.invoke(null, location, spawnReason);
+        } catch (Exception ignored) {}
         return null;
     }
 
