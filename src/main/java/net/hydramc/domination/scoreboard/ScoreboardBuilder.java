2ee1480da84c99cee1bea2e446bfbc3033ba880a
MrCubee <contact@mrcubee.fr>
2021-12-26 23:50:31 +0100

    Rework ScoreboardBuilder and ScoreboardManager.

--- src/main/java/net/hydramc/domination/scoreboard/ScoreboardBuilder.java
+++ src/main/java/net/hydramc/domination/scoreboard/ScoreboardBuilder.java
@@ -1,69 +1,36 @@
 package net.hydramc.domination.scoreboard;
 
-import fr.mrmicky.fastboard.FastBoard;
 import net.hydramc.domination.game.Game;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
-import java.util.HashMap;
-import java.util.Map;
-
+import java.util.*;
 
 public class ScoreboardBuilder {
 
+    public static List<String> build(Player player, Game game) {
+        List<String> lines = new ArrayList<String>();
 
-    private static final Map<Player, FastBoard> boards = new HashMap<>();
-
-    private static boolean isSetup(Player player) {
-        return boards.containsKey(player);
-    }
-
-    private static FastBoard createBoard(Player player) {
-        FastBoard board = new FastBoard(player);
-        boards.put(player, board);
-        return board;
-    }
-
-    private static FastBoard getBoard(Player player) {
-        if (!isSetup(player)) {
-            return createBoard(player);
+        if (game.getGameStats().ordinal() > 2) {
+            lines.addAll(Arrays.asList(
+                    "",
+                    "§7Jour: §b1",
+                    "",
+                    "§cRouge: " + "§f500",
+                    "§9Bleu: " + "§f500",
+                    ""
+            ));
+        } else {
+            lines.addAll(Arrays.asList(
+                    "",
+                    "§7Carte: §6Oasis",
+                    "§7Joueurs: §f" + Bukkit.getOnlinePlayers().size(),
+                    ""
+            ));
         }
-        return boards.get(player);
+
+        // TODO: Create scoreboard's sidebar line for a specific player.
+        lines.add("§eplay.hydramc.net");
+        return lines;
     }
-
-    public static void deleteBoard(Player player) {
-        boards.remove(player);
-    }
-
-    public static void build(Player player, Game game) {
-        FastBoard board = getBoard(player);
-        board.updateTitle("§3Domination");
-
-        switch (game.getGameStats()) {
-
-            case WAITING:
-                board.updateLines(
-                        "",
-                        "§7Carte: §6Oasis",
-                        "§7Joueurs: §f" + Bukkit.getOnlinePlayers().size(),
-                        "",
-                        "§eplay.hydramc.net"
-                );
-                break;
-
-            case DURING:
-                // TODO: In game scoreboard
-                board.updateLines(
-                        "",
-                        "§7Jour: §b1",
-                        "",
-                        "§cRouge: " + "§f500",
-                        "§9Bleu: " + "§f500",
-                        "",
-                        "§eplay.hydramc.net"
-                );
-                break;
-        }
-    }
-
 }
--- src/main/java/net/hydramc/domination/scoreboard/ScoreboardManager.java
+++ src/main/java/net/hydramc/domination/scoreboard/ScoreboardManager.java
@@ -1,17 +1,65 @@
 package net.hydramc.domination.scoreboard;
 
-import net.hydramc.domination.Domination;
+import fr.mrmicky.fastboard.FastBoard;
+import net.hydramc.domination.game.Game;
+import net.hydramc.domination.step.Step;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
+import java.util.List;
+import java.util.Map;
+import java.util.WeakHashMap;
+
 public class ScoreboardManager {
 
-    public static void updateAllPlayers() {
+    private final Game game;
+    private final Map<Player, FastBoard> playerBoards;
 
-        for (Player player : Bukkit.getOnlinePlayers()) {
-            ScoreboardBuilder.build(player, Domination.getGameInstance());
-        }
-
+    public ScoreboardManager(Game game) {
+        this.game = game;
+        this.playerBoards = new WeakHashMap<Player, FastBoard>();
     }
 
+    public FastBoard getOrCreate(Player player) {
+        FastBoard board;
+
+        if (player == null)
+            return null;
+        board = this.playerBoards.get(player);
+        if (board == null) {
+            board = new FastBoard(player);
+            this.playerBoards.put(player, board);
+        }
+        return board;
+    }
+
+    public boolean remove(Player player) {
+        FastBoard fastBoard;
+
+        if (player == null)
+            return false;
+        fastBoard = this.playerBoards.remove(player);
+        if (fastBoard == null)
+            return false;
+        fastBoard.delete();
+        return true;
+    }
+
+    public void update() {
+        final Step currentStep = game.getStepManager().getCurrentStep();
+        FastBoard fastBoard;
+        List<String> playerLines;
+
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            playerLines = (currentStep != null) ? currentStep.buildScoreboard(player, this.game)
+            : ScoreboardBuilder.build(player, this.game);
+
+            if (playerLines == null || playerLines.isEmpty())
+                remove(player);
+            else {
+                fastBoard = getOrCreate(player);
+                fastBoard.updateLines(playerLines);
+            }
+        }
+    }
 }
